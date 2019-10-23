433f11232a9cda11290fd995b49801381e2c937f
Lee <39004900+leewayjean@users.noreply.github.com>
2019-10-23 17:36:42 +0800

    调整路由结构

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,34 @@
+const Goods = () => import("../components/goods/Goods.vue")
+const Ratings = () => import("../components/ratings/Ratings.vue");
+const Comment = () => import("../components/ratings/rating/CommentAll.vue");
+const All1 = () => import("../components/ratings/rating/all2.vue")
+const All2 = () => import("../components/ratings/rating/all3.vue")
+const Seller = () => import("../components/seller/Seller.vue")
+
 
 export const routes = [
-	{path:"/",component:()=>import("../components/goods/Goods.vue")},
-	{path:"/ratings",component:()=>import("../components/ratings/Ratings.vue"),redirect:"/ratings/commentall",children:[
-		{path:"/ratings/commentall",component:()=>import("../components/ratings/rating/CommentAll.vue")},
-		{path:"/ratings/all2",component:()=>import("../components/ratings/rating/all2.vue")},
-		{path:"/ratings/all3",component:()=>import("../components/ratings/rating/all3.vue")},
-	]},
-	{path:"/sellers",component:()=>import("../components/seller/Seller.vue")},
-	{path:"*",component:()=>import("../components/goods/Goods.vue")}
+	{
+		path: "/", component: Goods
+	},
+	{
+		path: "/ratings", component: Ratings, redirect: "/ratings/commentall",
+		children: [
+			{
+				path: "/ratings/commentall", component: Comment
+			},
+			{
+				path: "/ratings/all2", component: All2
+			},
+			{
+				path: "/ratings/all3", component:All2
+			},
+		]
+	},
+	{
+		path: "/sellers", component: Seller
+	},
+	{
+		path: "*", component: Goods
+	}
 
 ]
